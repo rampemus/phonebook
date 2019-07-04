ee5fe0f472284b00c362e96d8485bb4699fe5c31
rampemus <pasi.toivanen@live.com>
2019-07-04 14:38:55 +0300

    Frontend build added

---
+++ build/precache-manifest.26aa8fe3a353e2bbf70b0b2fdb05b97e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d475aef3522ea33e24667b84bd7ffc40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "a5fe5037ab8ada8991e3",
+    "url": "/static/js/main.9f6f76e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
