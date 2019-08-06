69ff00c326555a03227e4c3cd0a197887b2728c4
rampemus <pasi.toivanen@live.com>
2019-08-06 15:30:04 +0300

    eslint: no-console errors removed

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,14 @@ module.exports = {
         ],
         "arrow-spacing": [
             "error", { "before": true, "after": true }
+        ],
+        "no-console": "off",
+        "no-restricted-syntax": [
+            "error",
+            {
+                "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+                "message": "Unexpected property on console object was called"
+            }
         ]
     }
 }
